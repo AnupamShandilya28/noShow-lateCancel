fc031d457b60e0ab8c68735387be7deb095e8841
akashbadhe-mb <akash.badhe@mindbodyonline.com>
2022-03-29 15:35:34 +0530

    AB#1_ updated story book

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import React, { useState } from 'react';
 import styles from './App.module.scss';
+import Counter from './Counter';
 
 export const App = () => {
     const [count, setCount] = useState(0);
     return (
-        <>
-            <div className={styles.count} data-testid={'count'}>
-                {count}
-            </div>
-            <button onClick={() => setCount(count + 1)}>Add</button>
-            <button onClick={() => setCount(count - 1)}>Subtract</button>
-        </>
+        <Counter />
     );
 };
 export default App;
