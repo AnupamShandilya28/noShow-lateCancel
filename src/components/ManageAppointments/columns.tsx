6bec030b093f69969e759bdf7c7587cd2cccc0ea
Interns-Strategic-Accounts <anupam.shandilya@mindbodyonline.com>
2022-04-20 13:27:25 +0530

    added manage appointement componenent

---
+++ src/components/ManageAppointments/columns.tsx
@@ -0,0 +1,27 @@
+export const COLUMNS=[
+    {
+        Header:"TYPE OF CLASS",
+        accessor:"TYPEOFCLASS"
+    },
+    {
+        Header:"TIME (PRIOR TO CLASS)",
+        accessor:"TIME"
+    },
+    {
+        Header:"LATE CANCEL",
+        accessor:"LATE CANCEL"
+    },
+    {
+        Header:"CHARGES",
+        accessor:"CHARGES_LC"
+    },
+    {
+        Header:"NO SHOW",
+        accessor:"NO SHOW"
+    },
+    {
+        Header:"CHARGES",
+        accessor:"CHARGES_NS"
+    },
+
+]
