99c6b8cbb3674b33d00008222450132eebce4b56
Nobody1909 <harshad.lokare@mindbodyonline.com>
2022-04-26 10:43:14 +0530

    static

--- src/components/ApplyCharges-Page/ApplyCharges.tsx
+++ src/components/ApplyCharges-Page/ApplyCharges.tsx
@@ -2,17 +2,12 @@ import React, { useState } from "react";
 import styles from "./ApplyCharges.module.scss";
 import { Label } from "@mbkit/label";
 import Main from "./MainModal/Main";
-import Content from "./Content";
 import Spinner from "./Spinner";
-import MemberDetails from '../MemberDetails/MemberDetails';
+import MemberDetails from "../MemberDetails/MemberDetails";
 const ApplyCharges = () => {
   const [showChargesPage, setShowChargesPage] = useState(false);
-  const [showLoader, setShowLoader] = useState(false);
+  const [showLoader, setShowLoader] = useState(false);;
 
- /*  const closeContentDiv = () => {
-    setShowChargesPage(false);
-  };
- */
   const searchButtonHandler = () => {
     setShowLoader(true);
     setTimeout(() => {
@@ -27,14 +22,14 @@ const ApplyCharges = () => {
         <h2 className={styles.head}>No Show/Late Cancel</h2>
         <Label>
           <span className={styles.head2}>Manager Tools </span>
-          <span className={styles.lab}>{">"}No Show/Late Cancel</span>
+          <span className={styles.lab}>{">"} No Show/Late Cancel</span>
         </Label>
 
-        <div className={styles.proDiv}>
+        <div className={styles.mainDiv}>
           <Main onSearchClick={searchButtonHandler}></Main>
         </div>
         {showLoader && <Spinner />}
-        {showChargesPage && <MemberDetails/> }
+        {showChargesPage && <MemberDetails/>}
       </div>
     </>
   );
