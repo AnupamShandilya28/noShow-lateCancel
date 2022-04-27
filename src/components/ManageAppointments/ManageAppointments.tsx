0a7ee70b7b1afb7f564f674fe89dc33adca841c6
Interns-Strategic-Accounts <anupam.shandilya@mindbodyonline.com>
2022-04-27 19:59:51 +0530

    converted into pure react-table form

--- src/components/ManageAppointments/ManageAppointments.tsx
+++ src/components/ManageAppointments/ManageAppointments.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import styles from "./styles/ManageAppointments.module.scss";
 import { useTable, useSortBy } from "react-table";
 import { COLUMNS } from "./columns";
@@ -8,8 +8,167 @@ import InfoIcon from "@material-ui/icons/Info";
 import Tablerow from "./Tablerow";
 import React from "react";
 import { Tipsy } from "@mbkit/tipsy";
+import { TipsyProps } from "@mbkit/tipsy/dist/cjs/Tipsy";
+import { Input, Checkbox } from "@mui/material";
 const ManageAppointments = () => {
-  const data = useMemo(() => MOCK_DATA, []);
+  const [data, setData] = useState(MOCK_DATA);
+  
+const onLateCancelHandler = () => {
+  // setIslateCancel(!isLateCancel);
+  // setLateCancelChargeInput(0);
+};
+const onNoShowHandler = () => {
+  // setIsNoShow(!isNoShow);
+  // setNoShowChargeInput(0);
+};
+const checkValidity = (value: string): boolean => {
+  for (let i = 0; i < value.length; i++) {
+    if (!(value[i] >= "0" && value[i] <= "9")) return false;
+  }
+  return true;
+};
+const onTimeChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+  if (!checkValidity(event.target.value)) return;
+  // setTimeInput(event.target.value);
+};
+const onLateCancelChargeHandler = (
+  event: React.ChangeEvent<HTMLInputElement>
+) => {
+  if (!checkValidity(event.target.value)) return;
+  // setLateCancelChargeInput(event.target.value);
+};
+const onNoShowChargeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+  if (!checkValidity(event.target.value)) return;
+  // setNoShowChargeInput(event.target.value);
+};
+  const COLUMNS= [
+    {
+      Header: "TYPE",
+      accessor: "TYPE",
+    },
+    {
+      Header: (
+        <>
+          <span>TIME (PRIOR TO CLASS)</span>
+          <Tipsy
+            position="top-center"
+            label={
+              <>
+                This is the time for cancelation before the class.
+                <br />
+                You can update this time from Cancelation Window
+              </>
+            }
+          >
+            <span>
+              <InfoIcon id={styles.infoicon} />
+            </span>
+          </Tipsy>
+        </>
+      ),
+      accessor:"TIME",
+      Cell: (value: number, row: any) => {                
+        return (
+          <input
+          onChange={onTimeChangeHandler}
+          type="string"
+          value={value}            
+          />
+        )    
+    }
+    },
+    {
+      // Header: "LATE CANCEL",
+      Header: (
+        <>
+          <span>LATE CANCEL</span>
+          <Tipsy
+            position="top-center"
+            label="Don't put anything critical to getting the task at hand complete in here"
+          >
+            <span>
+              <InfoIcon id={styles.infoicon} />
+            </span>
+          </Tipsy>
+        </>
+      ),
+      accessor:"LATECANCEL",
+      Cell: (rowData: { LATECANCEL: boolean; }, rowIndex:  number) => {
+          return (
+            <input
+              type="checkbox"
+              checked={rowData.LATECANCEL}
+              onClick={() => {
+                const tmpData = [...data];
+                tmpData[rowIndex].LATECANCEL = !tmpData[rowIndex].LATECANCEL;
+                setData(tmpData);
+              }}
+            />
+          )    
+      }
+    },
+    {
+      Header: "CHARGES",    
+      accessor:"CHARGES_LC",
+      Cell: (value: number, row: any) => {                
+        return (
+          <input
+          onChange={onTimeChangeHandler}
+          type="string"
+          value={value}            
+          />
+        )    
+    }
+    },
+    {
+      Header: (
+        <>
+          <span>NO SHOW</span>
+          <Tipsy
+            position="top-center"
+            label={
+              <>
+                If the consumer didn't cancel and was not present.
+                <br />
+                Apply the required charges.
+              </>
+            }
+          >
+            <span>
+              <InfoIcon id={styles.infoicon} />
+            </span>
+          </Tipsy>
+        </>
+      ),
+      accessor:"NOSHOW",
+      Cell: (rowData: { NOSHOW: boolean; }, rowIndex:  number) => {
+        return (
+          <input
+            type="checkbox"
+            checked={rowData.NOSHOW}
+            onClick={() => {
+              const tmpData = [...data];
+              tmpData[rowIndex].NOSHOW = !tmpData[rowIndex].NOSHOW;
+              setData(tmpData);
+            }}
+          />
+        )    
+    }
+    },
+    {
+      Header: "CHARGES",
+      accessor:"CHARGES_NS",
+      Cell: ({value}:{value:any}) => {
+        return (
+          <input
+          onChange={onNoShowChargeHandler}
+          // type="number"
+          value={value}            
+          />
+        )    
+    }
+    },
+  ];
   const columns = useMemo(() => processColumns(COLUMNS, data), [COLUMNS, data]);
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
     useTable({
@@ -26,72 +185,24 @@ const ManageAppointments = () => {
             <tr {...headerGroup.getHeaderGroupProps()}>
               {headerGroup.headers.map((column) => (
                 <th {...column.getHeaderProps()}>
-                  {column.render("Header")}
-                  {column.render("Header") === "TIME (PRIOR TO CLASS)" && (
-                    <Tipsy
-                      position="top-center"
-                      label={
-                        <>
-                          This is the time for cancelation before the class.
-                          <br />
-                          You can update this time from Cancelation Window
-                        </>
-                      }
-                    >
-                      <span>
-                        <InfoIcon id={styles.infoicon} />
-                      </span>
-                    </Tipsy>
-                  )}
-                  {column.render("Header") === "LATE CANCEL" && (
-                    <Tipsy
-                      position="top-center"
-                      label={
-                        <>
-                          If canceled after the stipulated time.
-                          <br />
-                          Apply the required charges.
-                        </>
-                      }
-                    >
-                      <span>
-                        <InfoIcon id={styles.infoicon} />
-                      </span>
-                    </Tipsy>
-                  )}
-                  {column.render("Header") === "NO SHOW" && (
-                    <Tipsy
-                      position="top-center"
-                      label={
-                        <>
-                          If the consumer didn't cancel and was not present.
-                          <br />
-                          Apply the required charges.
-                        </>
-                      }
-                    >
-                      <span>
-                        <InfoIcon id={styles.infoicon} />
-                      </span>
-                    </Tipsy>
-                  )}
-                  {/* {(column.render("Header") === "TIME (PRIOR TO CLASS)" ||
-                      column.render("Header") === "TYPE") && (
-                      <span>
-                        <InfoIcon id={styles.infoicon} />
-                      </span>
-                    )} */}
+                  {column.render("Header")}                  
                 </th>
               ))}
             </tr>
           ))}
         </thead>
         <tbody {...getTableBodyProps()}>
-          {rows.map((row, i) => {
-            prepareRow(row);
-            return <Tablerow key={i} row={row} />;
-          })}
-        </tbody>
+        {rows.map((row, i) => {
+          prepareRow(row)
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
       </table>
     </div>
   );
@@ -99,7 +210,11 @@ const ManageAppointments = () => {
 export default ManageAppointments;
 
 function processColumns(
-  COLUMNS: { Header: string; accessor: string }[],
+  COLUMNS: {
+    Header: string | JSX.Element;
+    accessor: String;
+    Cell?: (originalRow: any, rowIndex: any) => any;
+  }[],
   data: {
     id: number;
     TYPE: string;
--- src/components/ManageAppointments/columns.tsx
+++ src/components/ManageAppointments/columns.tsx
@@ -1,27 +1,172 @@
-export const COLUMNS=[
-    {
-        Header:"TYPE",
-        accessor:"TYPE"
-    },
-    {
-        Header:"TIME (PRIOR TO CLASS)",
-        accessor:"TIME"
-    },
-    {
-        Header:"LATE CANCEL",
-        accessor:"LATE CANCEL"
-    },
-    {
-        Header:"CHARGES",
-        accessor:"CHARGES_LC"
-    },
-    {
-        Header:"NO SHOW",
-        accessor:"NO SHOW"
-    },
-    {
-        Header:"CHARGES",
-        accessor:"CHARGES_NS"
-    },
+import MOCK_DATA from "../../data/manageAppointments/MOCK_DATA.json";
+import { Checkbox } from "@mbkit/checkbox";
+import { Input } from "@mbkit/input";
+import { Tipsy } from "@mbkit/tipsy";
+import React, { useState } from "react";
+import styles from "./styles/ManageAppointments.module.scss"
+import InfoIcon from "@mui/icons-material/Info";
 
-]
+const onLateCancelHandler = () => {
+  // setIslateCancel(!isLateCancel);
+  // setLateCancelChargeInput(0);
+};
+const onNoShowHandler = () => {
+  // setIsNoShow(!isNoShow);
+  // setNoShowChargeInput(0);
+};
+const checkValidity = (value: string): boolean => {
+  for (let i = 0; i < value.length; i++) {
+    if (!(value[i] >= "0" && value[i] <= "9")) return false;
+  }
+  return true;
+};
+const onTimeChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+  if (!checkValidity(event.target.value)) return;
+  // setTimeInput(event.target.value);
+};
+const onLateCancelChargeHandler = (
+  event: React.ChangeEvent<HTMLInputElement>
+) => {
+  if (!checkValidity(event.target.value)) return;
+  // setLateCancelChargeInput(event.target.value);
+};
+const onNoShowChargeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+  if (!checkValidity(event.target.value)) return;
+  // setNoShowChargeInput(event.target.value);
+};
+export const COLUMNS = [
+  {
+    Header: "TYPE",
+    accessor: "TYPE",
+  },
+  {
+    Header: (
+      <>
+        <span>TIME (PRIOR TO CLASS)</span>
+        <Tipsy
+          position="top-center"
+          label={
+            <>
+              This is the time for cancelation before the class.
+              <br />
+              You can update this time from Cancelation Window
+            </>
+          }
+        >
+          <span>
+            <InfoIcon id={styles.infoicon} />
+          </span>
+        </Tipsy>
+      </>
+    ),
+    accessor: "TIME",
+    Cell: (
+      {value}:{value:any}
+    ) => {
+      return (
+        <div>
+          <Input value={value} onChange={onTimeChangeHandler} />
+          <span>Minutes (Prior to class)</span>
+        </div>
+      );
+    },
+  },
+  {
+    // Header: "LATE CANCEL",
+    Header: (
+      <>
+        <span>LATE CANCEL</span>
+        <Tipsy
+          position="top-center"
+          label="Don't put anything critical to getting the task at hand complete in here"
+        >
+          <span>
+            <InfoIcon id={styles.infoicon} />
+          </span>
+        </Tipsy>
+      </>
+    ),
+    accessor: (rowData: { status: boolean | undefined; }, rowIndex:  number) => {
+        return (
+          <input
+            type="checkbox"
+            checked={rowData.status}
+            onClick={() => {
+              const tmpData = [...data];
+              tmpData[rowIndex].LATECANCEL = !tmpData[rowIndex].LATECANCEL;
+              setData(tmpData);
+            }}
+          />
+        )    
+    }
+  },
+  {
+    Header: "CHARGES",    
+    accessor: "CHARGES_LC",
+    Cell: (
+      value: (string | number | readonly string[] | undefined) & string
+    ) => {
+      return (
+        <div>
+          <Input
+            onChange={onLateCancelChargeHandler}
+            type="string"
+            value={value}
+            // disabled={!isLateCancel}
+          />
+        </div>
+      );
+    },
+  },
+  {
+    Header: (
+      <>
+        <span>NO SHOW</span>
+        <Tipsy
+          position="top-center"
+          label={
+            <>
+              If the consumer didn't cancel and was not present.
+              <br />
+              Apply the required charges.
+            </>
+          }
+        >
+          <span>
+            <InfoIcon id={styles.infoicon} />
+          </span>
+        </Tipsy>
+      </>
+    ),
+    accessor: "NOSHOW",
+    Cell: (value: boolean) => {
+      return (
+        <div>
+          <Checkbox checked={value} onChange={onNoShowHandler}></Checkbox>
+          <span>Enable</span>
+        </div>
+      );
+    },
+  },
+  {
+    Header: "CHARGES",
+    accessor: "CHARGES_NS",
+    Cell: (
+      value: (string | number | readonly string[] | undefined) & string
+    ) => {
+      return (
+        <div>
+          <Input
+            value={value}
+            onChange={onNoShowChargeHandler}
+            type="text"
+            // disabled={!isNoShow}
+          />
+        </div>
+      );
+    },
+  },
+];
+
+
+const [data, setData] = useState(MOCK_DATA);
